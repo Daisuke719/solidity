08de38bc86e85b3c291712be810392afd2362993
daisuke719 <daisuke719@ezweb.ne.jp>
2022-01-26 20:32:58 +0900

    とりま

--- chapter2/zombiefactory.sol
+++ chapter2/zombiefactory.sol
@@ -1,55 +1,36 @@
-//ゾンビゲームをサイトに合わせて作り、コードの説明をここにまとめる。
-//Chapter1が終わった時点のコード。
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
-//（solidityのバージョンを記載。エラーが起こらないように。）
+pragma solidity ^0.4.19;
 
 contract ZombieFactory {
-//ゾンビ軍団を生み出すためのコントラクト
-//（solidityのコードは全てコントラクト内にカプセル化されている。
-//コントラクトはイーサリアムアプリケーションの基本ブロックのこと。
-//変数やファンクションは全てコントラクトに属している。）
 
     event NewZombie(uint zombieId, string name, uint dna);
-    //eventはブロックチェーン上で何かが生じたときに、コントラクトがアプリのフロントエンドに伝えることができる。
-    //今回はゾンビを作る毎nそれをフロントエンドに伝えてアプリに表示する。
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-    //（符号なし整数uintは負の数ではないことを表す。
-    //状態変数はコントラクト内に永遠に保管される。つまりブロックチェーン上に記載される。
 
     struct Zombie {
-    //構造体の宣言
-
         string name;
         uint dna;
     }
 
     Zombie[] public zombies;
-    //（構造体の配列を作成し、publicで宣言）
 
-    function _createZombie(string memory _name, uint _dna) private {
-    //新しいゾンビを作成、関数はprivate、privateにするときは関数名の前にアンダーバー
-    //（関数の宣言、引数の設定）
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
+    function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        //新しいZombie構造体のオブジェクトを作ってそれを配列に格納
-
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
-    } 
+    }
 
-    function _generateRandomDna(string memory _str) private view returns (uint) {
-    //戻り値をuintに設定
+    function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
-        //16桁のDNAを作る
-        //ハッシュ関数（不完全）を用いてハッシュ値を生成。uintにキャスト
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string memory _name) public {
-    //ゾンビの名前やユーザーの名前をインプットしてランダムなDNAでゾンビを作る
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
