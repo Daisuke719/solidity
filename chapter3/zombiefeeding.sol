68b647684531b79e3c08389a6a65161e4bbc1f20
daisuke719 <daisuke719@ezweb.ne.jp>
2022-01-30 18:05:13 +0900

    chapter3_fin

--- chapter3/zombiefeeding.sol
+++ chapter3/zombiefeeding.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+ragma solidity ^0.4.19;
+
 import "./zombiefactory.sol";
+
 contract KittyInterface {
-//インターフェースを定義
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
@@ -16,10 +16,14 @@ contract KittyInterface {
     uint256 genes
   );
 }
+
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
+
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
